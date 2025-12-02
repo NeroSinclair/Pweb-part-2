2b6a42d76f0cb2effb96f7bce396c4c2f3ecc25d
ItsNero-ir <fitranaufal7037@gmail.com>
2025-12-02 20:30:13 +0700

    Upload folder Pweb

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div className="flex justify-center mt-10">
+      <Counter initialNumber={10} title="Counter Angka Fitra" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/counter.jsx
@@ -0,0 +1,33 @@
+//nama fitra naufal eka//
+
+import { useState } from "react";
+
+function Counter({ initialNumber, title }) {
+  const [number, setNumber] = useState(initialNumber);
+
+  return (
+    <div className="bg-slate-800 text-white p-10 rounded-lg text-center">
+      <h1 className="text-2xl font-bold mb-5">{title}</h1>
+
+      <h2 className="text-4xl mb-5">{number}</h2>
+
+      <div className="flex justify-center gap-4">
+        <button
+          className="bg-blue-500 p-3 rounded-md"
+          onClick={() => setNumber(number + 1)}
+        >
+          Tambah
+        </button>
+
+        <button
+          className="bg-red-600 p-3 rounded-md"
+          onClick={() => setNumber(number - 1)}
+        >
+          Kurang
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
